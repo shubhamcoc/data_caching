d7028672698555938bfaab6f8556406667ae1de4
shubham kar <shubhamkar77@gmail.com>
2020-10-04 00:28:21 +0530

    Bug fixes in node port service and python test code
    Signed-off-by: shubham kar <shubhamkar77@gmail.com>

--- deploy/test/server.py
+++ deploy/test/server.py
@@ -7,7 +7,7 @@ from werkzeug import serving
 APP = Flask(__name__)
 @APP.route("/submit", methods=['GET', 'POST'])
 def send_submit():
-    url = "http://localhost:30007/api/submit"
+    url = "http://127.0.0.1:30007/api/submit"
     id = 123400
     num = 5
     for i in range(0, 99):
